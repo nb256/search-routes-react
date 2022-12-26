9e6dc28f8b5ae41d0a1e1ae3815e8033c9751150
Naim Buru <nbur4u@gmail.com>
2022-12-26 12:58:50 +0300

    fixup! feat: add calculateDistances function and its tests

--- src/api/calculateDistances.spec.ts
+++ src/api/calculateDistances.spec.ts
@@ -1,21 +1,21 @@
 import calculateDistances from "./calculateDistances";
 
 describe("calculateDistances", () => {
-  it("should return an empty array when no cities are given", () => {
+  it("should return an empty array when no cities are given", async () => {
     const cities = [] as string[];
-    const result = calculateDistances(cities);
+    const result = await calculateDistances(cities);
     expect(result).toEqual([]);
   });
 
-  it("should return an empty array when only one city is given", () => {
+  it("should return an empty array when only one city is given", async () => {
     const cities = ["Marseille"];
-    const result = calculateDistances(cities);
+    const result = await calculateDistances(cities);
     expect(result).toEqual([]);
   });
 
-  it("should return an array of distances between cities", () => {
+  it("should return an array of distances between cities", async () => {
     const cities = ["Marseille", "Paris", "Lyon"];
-    const result = calculateDistances(cities);
+    const result = await calculateDistances(cities);
     expect(result).toEqual([660480.5742037299, 391493.78309834626]);
   });
 });
